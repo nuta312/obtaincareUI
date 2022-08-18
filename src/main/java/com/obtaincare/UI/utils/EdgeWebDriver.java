ae9d093c6b82e5fb0d23ca282d9b8148cd758fc5
Kairat <maratovkajr@gmail.com>
2022-08-18 14:46:28 +0600

    kairats first commit UI frameWork

--- src/main/java/com/obtaincare/UI/utils/EdgeWebDriver.java
+++ src/main/java/com/obtaincare/UI/utils/EdgeWebDriver.java
@@ -1,23 +1,2 @@
-package com.obtaincare.UI.utils;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-
-import java.time.Duration;
-
-public class EdgeWebDriver {
-
-
-    public static WebDriver loadEdgeDriver(){
-
-        WebDriver driver;
-        WebDriverManager.edgedriver().setup();
-        driver = new EdgeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.manage().deleteAllCookies();
-
-        return driver;
-    }
+package com.obtaincare.UI.utils;public class EdgeWebDriver {
 }
--- src/test/java/UI/runner/RunnerClass.java
+++ src/test/java/UI/runner/RunnerClass.java
@@ -1,23 +1,2 @@
-package UI.runner;
-
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
-
-@RunWith(Cucumber.class)
-@CucumberOptions(
-        features = "src/test/resources/features",
-        glue = "UI/stepDefs",
-        tags = "@smoke",
-        dryRun = false,
-        monochrome = true,
-        publish = true,
-        plugin = {
-                "pretty",
-                "html:target/default-cucumber-reports",
-                "json:target/cucumber.json"
-        }
-)
-public class RunnerClass {
-
+package UI.runner;public class RunnerClass {
 }
